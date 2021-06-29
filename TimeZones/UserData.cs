80805590efeab89075e855a53b26de62f3749b8c
Stuart Rankin <sjrankin@gmail.com>
2021-06-28 19:43:03 -0700

    Create UserData.cs
    Placeholder for database access.

---
+++ TimeZones/UserData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Diagnostics;
+
+namespace TimeZones
+{
+    public static class UserData
+    {
+        public static bool InitializeDatabase()
+        {
+            string cs = "Data Source=:memory:";
+            string stm = "SELECT SQLITE_VERSION()";
+
+            using var con = new SQLiteConnection(cs);
+            con.Open();
+
+            using var cmd = new SQLiteCommand(stm, con);
+            string version = cmd.ExecuteScalar().ToString();
+
+            Debug.WriteLine($"SQLite version: {version}");
+            return true;
+        }
+    }
+}
