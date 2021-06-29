04d323a8988e0c3664ff891f21f96cabb453c1b6
Stuart Rankin <sjrankin@gmail.com>
2021-06-28 19:45:06 -0700

    Create TimeZoneManager.cs
    Placeholder for time zone data.

---
+++ TimeZones/TimeZoneManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace TimeZones
+{
+    class TimeZoneManager
+    {
+        TimeZoneManager()
+        {
+            //TimeZoneData = LoadTimeZoneData();
+        }
+
+//        ObservableCollection<TimeZone> LoadTimeZoneData()
+//        {
+//            return ObservableCollection<TimeZone>();
+//        }
+
+        public ObservableCollection<TimeZone> TimeZoneData { get; set; }
+    }
+
+    class TimeZone
+    {
+        public string ID { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
+
